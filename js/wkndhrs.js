96c92accec6a8925c08949da381d57ca82dda998
aztorius-j <juro.suchon@gmail.com>
2025-03-02 22:37:08 +0100

    big-text animation in progress...

--- js/wkndhrs.js
+++ js/wkndhrs.js
@@ -1,20 +1,43 @@
 const   wkndhrs = document.getElementById('wkndhrs'),
-        smallText = document.querySelector('.small-text');
-        lines = Array.from(document.querySelectorAll('.line'));
+        smallText = document.querySelector('.small-text'),
+        lines = Array.from(document.querySelectorAll('.line')),
+        serviceItems = Array.from(document.querySelectorAll('.service-item'));
 
-let     scrollStartingPoint = wkndhrs.previousElementSibling.offsetHeight + (smallText.clientHeight / 2);
-let     backwardScrollingPoint = wkndhrs.previousElementSibling.offsetHeight + (smallText.clientHeight);
-let     animationTriggered = false;
+let     scrollPointOne = wkndhrs.previousElementSibling.offsetHeight,
+        scrollPointTwo = scrollPointOne + (smallText.clientHeight / 2),
+        scrollPointThree = scrollPointOne + wkndhrs.clientHeight;
+        animationTriggered = false;
 
+// SMALL TEXT INITIALIZATION
 lines.forEach((line, index) => {
-    const offset = Math.pow(index, 1.5) * 2;
+    const offset = (index ** 1.5) * 2;
     line.style.transform = `translateY(${offset}px)`;
 });
 
-const triggerAnimation = () => {
-    const scrollTop = window.scrollY + window.innerHeight;
+// BIG TEXT INITIALIZATION
+let startingOffsets = [
+    window.innerWidth / 4 + 50,
+    window.innerWidth / 4 + 200,
+    window.innerWidth / 4 + 350,
+    window.innerWidth / 4 + 500
+];
 
-    if (scrollTop >= scrollStartingPoint && !animationTriggered) {
+let endingOffsets = [
+    scrollPointThree - 300,
+    scrollPointThree - 200, 
+    scrollPointThree - 100,  
+    scrollPointThree
+];
+
+serviceItems.forEach((item, index) => {
+    item.style.transform = `translateX(${startingOffsets[index]}px)`;
+});
+
+// ANIMATIONS
+const triggerAnimation = () => {
+    const scrollBottom = window.scrollY + window.innerHeight;
+
+    if (scrollBottom >= scrollPointTwo && !animationTriggered) {
         animationTriggered = true;
         document.documentElement.style.setProperty("--scale-value", 0);
         lines.forEach((line, i) => {
@@ -23,6 +46,16 @@ const triggerAnimation = () => {
             }, i * 25);
         });
     }
+
+    serviceItems.forEach((item, index) => {
+        if (scrollBottom >= scrollPointOne && scrollBottom <= endingOffsets[index]) {
+            let progress = (scrollBottom - scrollPointOne) / (endingOffsets[index] - scrollPointOne);
+            progress = Math.min(progress, 1);
+
+            let currentOffset = startingOffsets[index] * (1 - progress);
+            item.style.transform = `translateX(${currentOffset}px)`;
+        }
+    });
 };
 
 window.addEventListener("scroll", triggerAnimation);
