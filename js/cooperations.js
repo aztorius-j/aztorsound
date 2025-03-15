69a6c02f1905e0386e3681e3f66e5c2528ec2a09
aztorius-j <juro.suchon@gmail.com>
2025-03-15 21:24:55 +0100

    media queries added to cooperations.css

--- js/cooperations.js
+++ js/cooperations.js
@@ -3,7 +3,6 @@ const cooperationsSection = document.getElementById('cooperations'),
       artistContainer = document.querySelector('.artists-container'),
       artistsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
-let isScrolling = false;
 let startingPoint, totalProgress, sectionHeight;
 
 artistsArray.forEach((artist) => {
@@ -25,22 +24,14 @@ const updateValues = () => {
 };
 
 const artistScroll = () => {
-    if (!isScrolling) return; // Ak sa nescrolluje, nerob nič
-
     let progress = Math.max(0, window.scrollY - startingPoint);
     let progressPercentage = Math.min(progress / totalProgress, 1);
     const translateX = -progressPercentage * totalProgress;
     cooperationsWrapper.style.transform = `translateX(${translateX}px)`;
-
-    requestAnimationFrame(artistScroll);
 };
 
+window.addEventListener("scroll", artistScroll);
 window.addEventListener("resize", updateValues);
-window.addEventListener("scroll", () => {
-    isScrolling = true;
-    requestAnimationFrame(artistScroll);
-    setTimeout(() => isScrolling = false, 100); // Po krátkom čase sa vypne
-});
 
 updateValues();
 artistScroll();
