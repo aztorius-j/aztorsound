bc5d4353f394bac2f96e8492960273b69a9c47d8
aztorius-j <juro.suchon@gmail.com>
2025-03-15 15:18:54 +0100

    small progress in cooperations section...

--- js/cooperations.js
+++ js/cooperations.js
@@ -1,11 +1,44 @@
-const artistsWrapper = document.querySelector('.artists-wrapper');
-const artistsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+const   cooperationsWrapper = document.querySelector('.cooperations-wrapper'),
+        cooperationsSection = document.getElementById('cooperations');
+        fakeElement = Array.from(document.querySelectorAll('.fake-element')),
+        artistContainer = document.querySelector('.artists-container'),
+        artistsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
-artistsArray.forEach((artist) => {
+let     startingPoint,
+        totalProgress = cooperationsSection.clientHeight;
+
+artistsArray.forEach((artist, index) => {
     let artistFrame = document.createElement('div');
     artistFrame.classList.add('artist');
     artistFrame.innerText = artist;
-    artistsWrapper.appendChild(artistFrame);
+    artistContainer.appendChild(artistFrame);
 });
 
+const updateValues = () => {
+    startingPoint = cooperationsSection.getBoundingClientRect().top + window.scrollY;
+    const fakeOffset = (window.innerWidth - 400) / 2;
 
+fakeElement.forEach((element, index) => {
+        element.style.width = `${fakeOffset}px`;
+});
+
+    artistScroll();
+};
+
+const artistScroll = () => {
+    // const viewportBottom = window.scrollY + window.innerHeight;
+
+    if (window.scrollY >= startingPoint) {
+        let progress = window.scrollY - startingPoint;
+        let progressPercentage = Math.min(progress / totalProgress, 0.87);
+
+        const translateX = -progressPercentage * totalProgress;
+        cooperationsWrapper.style.transform = `translateX(${translateX}px)`;
+    }
+
+    requestAnimationFrame(artistScroll);
+};
+
+window.addEventListener("resize", updateValues);
+updateValues();
+artistScroll();
