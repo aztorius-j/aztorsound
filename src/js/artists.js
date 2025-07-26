7f790ed46e242ae6346192a1b9c7627de2f054a2
aztorius-j <juro.suchon@gmail.com>
2025-07-26 12:44:37 +0200

    style: adjust indentation from 4 to 2 spaces across project for consistency

--- src/js/artists.js
+++ src/js/artists.js
@@ -1,62 +1,62 @@
 export const artistsArray = [
-    {
-        name: 'NEFKUS',
-        type: 'creative agency',
-        poster: '/img/cooperations/nefkus-grayscale.webp'
-    },
-    {
-        name: 'CUBE to CUBE',
-        type: 'creative agency',
-        poster: '/img/cooperations/cube-grayscale.webp'
-    },
-    {
-        name: 'Anna Kvasnovska',
-        type: 'voice actress',
-        poster: '/img/cooperations/anna-kvasnovska-grayscale.webp'
-    },
-    {
-        name: 'Bystrik',
-        type: 'singer, band leader',
-        poster: '/img/cooperations/bystrik-grayscale.webp'
-    },
-    {
-        name: 'Stolen Street',
-        type: 'indie band',
-        poster: '/img/cooperations/stolen-street-grayscale.webp'
-    },
-    {
-        name: 'Ivan Zadrabaj',
-        type: 'pianist, composer',
-        poster: '/img/cooperations/ivan-zadrabaj-grayscale.webp'
-    },
-    {
-        name: 'Ychabods',
-        type: "rock'n'roll band",
-        poster: '/img/cooperations/ychabods-grayscale.webp'
-    },
-    {
-        name: 'The Nosebleeds',
-        type: 'garage rock band',
-        poster: '/img/cooperations/nosebleeds-grayscale.webp'
-    },
-    {
-        name: 'Lukas Pista',
-        type: 'singer, actor',
-        poster: '/img/cooperations/lukas-pista-grayscale.webp'
-    },
-    {
-        name: 'Triple Jump',
-        type: 'jazz band',
-        poster: '/img/cooperations/triple-jump-grayscale.webp'
-    },
-    {
-        name: 'Hurooonky',
-        type: 'podcasters',
-        poster: '/img/cooperations/hurooonky-grayscale.webp'
-    },
-    {
-        name: 'Lubomir Mazak',
-        type: 'producer',
-        poster: '/img/cooperations/lubo-mazak-grayscale.webp'
-    }
+  {
+    name: 'NEFKUS',
+    type: 'creative agency',
+    poster: '/img/cooperations/nefkus-grayscale.webp'
+  },
+  {
+    name: 'CUBE to CUBE',
+    type: 'creative agency',
+    poster: '/img/cooperations/cube-grayscale.webp'
+  },
+  {
+    name: 'Anna Kvasnovska',
+    type: 'voice actress',
+    poster: '/img/cooperations/anna-kvasnovska-grayscale.webp'
+  },
+  {
+    name: 'Bystrik',
+    type: 'singer, band leader',
+    poster: '/img/cooperations/bystrik-grayscale.webp'
+  },
+  {
+    name: 'Stolen Street',
+    type: 'indie band',
+    poster: '/img/cooperations/stolen-street-grayscale.webp'
+  },
+  {
+    name: 'Ivan Zadrabaj',
+    type: 'pianist, composer',
+    poster: '/img/cooperations/ivan-zadrabaj-grayscale.webp'
+  },
+  {
+    name: 'Ychabods',
+    type: "rock'n'roll band",
+    poster: '/img/cooperations/ychabods-grayscale.webp'
+  },
+  {
+    name: 'The Nosebleeds',
+    type: 'garage rock band',
+    poster: '/img/cooperations/nosebleeds-grayscale.webp'
+  },
+  {
+    name: 'Lukas Pista',
+    type: 'singer, actor',
+    poster: '/img/cooperations/lukas-pista-grayscale.webp'
+  },
+  {
+    name: 'Triple Jump',
+    type: 'jazz band',
+    poster: '/img/cooperations/triple-jump-grayscale.webp'
+  },
+  {
+    name: 'Hurooonky',
+    type: 'podcasters',
+    poster: '/img/cooperations/hurooonky-grayscale.webp'
+  },
+  {
+    name: 'Lubomir Mazak',
+    type: 'producer',
+    poster: '/img/cooperations/lubo-mazak-grayscale.webp'
+  }
 ];
--- src/js/cooperations.js
+++ src/js/cooperations.js
@@ -3,23 +3,23 @@ const artistContainer = document.querySelector('.artists-container');
 import { artistsArray } from './artists.js';
 
 const shuffleArray = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 };
 
 shuffleArray(artistsArray);
 
 artistsArray.forEach((artist) => {
-    let artistFrame = document.createElement('div');
-    artistFrame.classList.add('artist');
-    artistFrame.style.background = `url(${artist.poster}) 50% 50% / contain no-repeat`;
+  let artistFrame = document.createElement('div');
+  artistFrame.classList.add('artist');
+  artistFrame.style.background = `url(${artist.poster}) 50% 50% / contain no-repeat`;
 
-    let artistDescription = document.createElement('div');
-    artistDescription.classList.add('artist-description');
-    artistDescription.innerHTML = `<h5>${artist.name} <small>/${artist.type}/</small></h5>`;
+  let artistDescription = document.createElement('div');
+  artistDescription.classList.add('artist-description');
+  artistDescription.innerHTML = `<h5>${artist.name} <small>/${artist.type}/</small></h5>`;
 
-    artistFrame.appendChild(artistDescription);
-    artistContainer.appendChild(artistFrame);
+  artistFrame.appendChild(artistDescription);
+  artistContainer.appendChild(artistFrame);
 });
