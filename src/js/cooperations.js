52f07d52047833a7a4fd7c2af2922e53f8b3cdf3
aztorius-j <juro.suchon@gmail.com>
2025-07-27 10:25:06 +0200

    feat(cooperations): implemented scale and opacity animations; section fully completed

--- src/js/cooperations.js
+++ src/js/cooperations.js
@@ -23,3 +23,24 @@ artistsArray.forEach((artist) => {
   artistFrame.appendChild(artistDescription);
   artistList.appendChild(artistFrame);
 });
+
+// SCALE AND OPACITY LOGIC
+const artistScroll = () => {
+    const viewportCenter = window.innerWidth / 2;
+    const artistElements = Array.from(document.querySelectorAll('.artist'));
+
+    artistElements.forEach(artist => {
+        const artistRect = artist.getBoundingClientRect();
+        const artistCenter = artistRect.left + artistRect.width / 2;
+        const distanceFromCenter = Math.abs(viewportCenter - artistCenter);
+        const maxDistance = viewportCenter;
+        const scale = Math.max(1, 1.04 - (distanceFromCenter / maxDistance) * 0.05);
+        artist.style.transform = `scale(${scale})`;
+
+        const opacity = Math.max(0, 1 - (distanceFromCenter / maxDistance));
+        const description = artist.querySelector('.artist-description');
+        description.style.opacity = opacity;
+    });
+};
+
+window.addEventListener('scroll', artistScroll);
